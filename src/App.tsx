656dcada9bbf27f20e3d7f73be12455de20baf1f
Danielle Underwood <50803367+danielleunderwood@users.noreply.github.com>
2024-12-30 00:36:49 +0000

    remove 1

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
       <div className="w-full max-w-[48rem] min-w-0">
         <SongCard
           url="https://soundcloud.com/jaiwolfmusic/track-5"
-          text="This song reminded me of you! 1"
+          text="This song reminded me of you!"
         />
       </div>
     </>
