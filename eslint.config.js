72b9ca7116ee52bd407ddbc97b8608f6294e613e
Danielle Underwood <50803367+danielleunderwood@users.noreply.github.com>
2025-01-03 21:29:20 +0000

    fix eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,21 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
+import eslint from "@eslint/js";
+import pluginHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import eslintPluginJsxA11y from "eslint-plugin-jsx-a11y";
 
 export default [
-  tseslint.config(
-    { ignores: ["dist"] },
-    {
-      languageOptions: {
-        ecmaVersion: 2020,
-        globals: globals.browser,
-      },
-      plugins: {
-        "react-hooks": reactHooks,
-        "react-refresh": reactRefresh,
-      },
-      extends: [js.configs.recommended, ...tseslint.configs.recommended],
-      files: ["**/*.{ts,tsx}"],
-      rules: {
-        ...reactHooks.configs.recommended.rules,
-        "react-refresh/only-export-components": [
-          "warn",
-          { allowConstantExport: true },
-        ],
-      },
-    }
-  ),
+  ...tseslint.config(eslint.configs.recommended, tseslint.configs.recommended),
+  eslintPluginJsxA11y.flatConfigs.recommended,
+  reactRefresh.configs.vite,
+  {
+    plugins: {
+      "react-hooks": pluginHooks,
+    },
+    rules: {
+      ...pluginHooks.configs.recommended.rules,
+    },
+  },
   eslintPluginPrettierRecommended,
 ];
