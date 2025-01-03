7a50ed12f9cd5010d272c621da9dbe64c90da912
Danielle Underwood <50803367+danielleunderwood@users.noreply.github.com>
2025-01-03 21:29:57 +0000

    switch to menu logout

--- src/TopNav.tsx
+++ src/TopNav.tsx
@@ -1,15 +1,21 @@
-import { AppBar, Toolbar, useScrollTrigger } from "@mui/material";
-import { cloneElement } from "react";
+import {
+  AppBar,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  useScrollTrigger,
+} from "@mui/material";
+import { cloneElement, useState, MouseEvent } from "react";
 import supabase from "./utils/supabase";
-import Button from "./components/Button";
 import useStore from "./store";
+import { UserCircleIcon } from "@heroicons/react/24/solid";
 
 interface ElevationScrollProps {
   children?: React.ReactElement<{ elevation?: number }>;
 }
 
-function ElevationScroll(props: ElevationScrollProps) {
-  const { children } = props;
+function ElevationScroll({ children }: ElevationScrollProps) {
   const trigger = useScrollTrigger({
     disableHysteresis: true,
     threshold: 0,
@@ -24,28 +30,64 @@ function ElevationScroll(props: ElevationScrollProps) {
 
 function TopNav() {
   const { session } = useStore();
+
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const logout = async () => {
+    await supabase.auth.signOut();
+
+    handleClose();
+  };
+
   return (
     <>
       <ElevationScroll>
         <AppBar>
           <Toolbar className="bg-white dark:bg-zinc-900">
-            <div className="flex justify-between w-full">
-              <div
-                style={{ fontFamily: "Doto, monospace" }}
-                className="bg-black dark:bg-transparent text-white px-4 py-2"
-              >
-                music-sns
-              </div>
-              {session && (
-                <Button
-                  type="button"
-                  onClick={async () => {
-                    await supabase.auth.signOut();
-                  }}
+            <div className="flex justify-between items-center w-full">
+              <div>
+                <div
+                  style={{ fontFamily: "Doto, monospace" }}
+                  className="bg-black dark:bg-transparent text-white px-4 py-2"
                 >
-                  <div className="px-2">Logout</div>
-                </Button>
-              )}
+                  music-sns
+                </div>
+              </div>
+              <div className="flex">
+                <IconButton
+                  type="button"
+                  onClick={handleClick}
+                  className="h-full"
+                  aria-controls={open ? "basic-menu" : undefined}
+                  aria-haspopup="true"
+                  aria-expanded={open ? "true" : undefined}
+                >
+                  <UserCircleIcon className="h-10 w-10 text-black dark:text-white" />
+                </IconButton>
+                {session && (
+                  <Menu
+                    id="basic-menu"
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                    MenuListProps={{
+                      "aria-labelledby": "basic-button",
+                    }}
+                  >
+                    <MenuItem onClick={logout}>Logout</MenuItem>
+                  </Menu>
+                )}
+              </div>
             </div>
           </Toolbar>
         </AppBar>
