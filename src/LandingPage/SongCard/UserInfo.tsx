e6776859a6928273b65bbaf97ba76d5c21b88a95
Danielle Underwood <50803367+danielleunderwood@users.noreply.github.com>
2025-01-09 13:49:29 +0000

    skeleton loader for user info

--- src/LandingPage/SongCard/UserInfo.tsx
+++ src/LandingPage/SongCard/UserInfo.tsx
@@ -1,4 +1,3 @@
-import { Alert, LinearProgress } from "@mui/material";
 import useAsyncEffect from "../../hooks/useAsyncEffect";
 import { Status } from "../../types/status";
 import supabase from "../../utils/supabase";
@@ -8,7 +7,7 @@ interface UserInfoProps {
   userId: string;
 }
 function UserInfo({ userId }: UserInfoProps) {
-  const { value, status } = useAsyncEffect({
+  const { value: profile, status } = useAsyncEffect({
     effect: async () => {
       const result = await supabase.from("profiles").select().eq("id", userId);
 
@@ -27,14 +26,25 @@ function UserInfo({ userId }: UserInfoProps) {
   });
 
   if (status === Status.Loading) {
-    return <LinearProgress />;
+    return (
+      <div
+        aria-busy
+        aria-hidden
+        className="text-sm bg-gray-500 animate-pulse rounded-sm h-4 w-1/4"
+      />
+    );
   }
 
-  if (status === Status.Error || !value) {
-    return <Alert severity="error">Failed to load poster info.</Alert>;
+  if (status === Status.Error || !profile) {
+    return (
+      <div
+        aria-description="Failed to load user info"
+        className="text-sm bg-red-300 dark:bg-red-700 rounded-sm"
+      />
+    );
   }
 
-  return <div className="text-sm text-gray-500">{value.display_name}</div>;
+  return <div className="text-sm text-gray-500">{profile.display_name}</div>;
 }
 
 export default UserInfo;
